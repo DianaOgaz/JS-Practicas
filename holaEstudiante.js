9636c0d7bf70033ff6aef9ba4f8448c2a6226a8f
~ L U N A <dianaogaz@grupopromessa.com>
2023-10-05 10:32:46 -0600

    scope y más funciónes

---
+++ holaEstudiante.js
@@ -0,0 +1,10 @@
+
+function saludarEstudiante(nombre) {
+    console.log('Bienvenido ' + nombre);
+}
+saludarEstudiante('Luna');
+
+function saludarEstudiante(nombre) {
+    console.log(`Bienvenido ${nombre}`);
+}
+saludarEstudiante('Luna');
---
+++ scope.js
@@ -0,0 +1,10 @@
+var miNombre = 'Diana';
+
+function nombre() {
+var miApellido = 'Ogaz Escarpita';
+return miApellido;
+
+}
+//Imprimimos con el metodo de interpolacion y al final agregamos la llamada a la funcion. 
+console.log(`mi nombre es ${miNombre} y mi apellido es`, nombre());
+
