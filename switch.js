afbb0d245cd37fcfec1a39339950f37006b98476
~ L U N A <dianaogaz@grupopromessa.com>
2023-10-06 13:51:23 -0600

    practicas con switchs

--- switch.js
+++ switch.js
@@ -1,8 +1,13 @@
 
-var color = 'Rojo';
+var color = 'morado';
 
-switch (color){
+switch (color) {
     case 'verde': console.log('Avanza!'); break;
     case 'Amarillo': console.log('Bajale!'); break;
     case 'Rojo': console.log('Detende!'); break;
+    default: {
+        if (color !== 'verde' && 'Rojo' && 'Amarillo') {
+            console.log('error!');
+        }//if
+    }//switch
 }
---
+++ switchP.js
@@ -0,0 +1,29 @@
+var article = 'computadora';
+
+
+// Tu código aquí 👈
+switch (article) {
+    case 'computadora': {
+        console.log("Con mi computadora puedo programar usando JavaScript");
+        break;
+    }
+
+    case "celular": {
+        console.log("En mi celular puedo aprender usando la app de Platzi");
+        break;
+    }
+
+    case "cable": {
+        console.log("¡Hay un cable en mi bota!");
+        break;
+    }
+
+    default: {
+        if (article != "computadora" && "celular" && "cable") {
+            console.log("Artículo no encontrado");
+        }
+        break;
+    }
+
+}
+
