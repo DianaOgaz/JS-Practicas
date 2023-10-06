045f52bff1b07658fd96cc0de144510413b3d446
~ L U N A <dianaogaz@grupopromessa.com>
2023-10-06 10:40:44 -0600

    e añadiò reto de juego de piedra papel y tijeras

--- juego.js
+++ juego.js
@@ -1,12 +1,35 @@
+
 var piedra = 1;
 var papel = 2;
 var tijeras = 3;
 
-function juego (number, user){
-    var cpu = Math.random() *10 ;
 
-    if (number = cpu) return piedra;
-    if (number = cpu) return papel;
-    if (number = cpu) return tijeras;
+var iUser = Math.floor(Math.random() * 3) + 1;
+juego(iUser);
+
+function juego(iUser) {
+    var cpu = Math.floor(Math.random() * 3) + 1;
+    var jugador;
+
+    if (cpu == 1 && iUser == 3) {
+        console.log('Ganò CPU!');
+    } else if (iUser == 1 && cpu == 3) {
+        console.log('Ganò jugador!');
+    }
+    if (cpu == 2 && iUser == 1) {
+        console.log('Ganò CPU!');
+    } else if (iUser == 2 && cpu == 1) {
+        console.log('Ganò jugador!');
+    }
+    if (cpu == 3 && iUser == 2) {
+        console.log('Ganò CPU!');
+    } else if (iUser == 3 && cpu == 2) {
+        console.log('Ganò jugador!');
+    }
+    if (cpu == iUser) {
+        console.log('Empate!');
+    }
+
+
 
 }
